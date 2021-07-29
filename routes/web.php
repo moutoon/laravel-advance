17d505ab29769ff9bd7609a1e2263bc0a493b7ec
moutoon <moutoonm342@gmail.com>
2021-07-29 07:37:58 +0000

    Add: 新規ユーザ登録フォームを作成

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,10 @@
 |
 */
 
-// rootにGET -> View welcome.blade.php を表示
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ユーザー登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
