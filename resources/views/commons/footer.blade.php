9efc7bbb613e529a994df09a66b1ce6b15bd0880
moutoon <moutoonm342@gmail.com>
2021-07-29 06:21:34 +0000

    update: welcome.blade.php
    トップページ完成

--- resources/views/commons/footer.blade.php
+++ resources/views/commons/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="mt-5">
-    <nav class="navbar navbar-light bg-light justify-coutent-center">
+    <nav class="navbar navbar-light bg-light justify-content-center">
         <div class="navbar-brand m-0">© QuestAcademia, All rights reserved.</div>
     </nav>
 </footer>
