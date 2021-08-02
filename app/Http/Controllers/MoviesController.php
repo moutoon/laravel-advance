cf92ea41104a3f97d02c7b6c205b75b0719eedb2
moutoon <moutoonm342@gmail.com>
2021-08-02 14:09:50 +0000

    add procfile

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -23,6 +23,8 @@ class MoviesController extends Controller
     
     public function store(Request $request)
     {
+        dd($request);
+        
         $this->validate($request, [
             'url' => 'required|max:11',
             'comment' => 'max:36',
